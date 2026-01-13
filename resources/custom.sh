4dc50eecc46a96be1148390f5ede6d9c54bf2c75
MuldeR <mulder2@gmx.de>
2026-01-13 20:13:39 +0100

    Added the required dependencies + force update of installed packages.

--- resources/custom.sh
+++ resources/custom.sh
@@ -3,3 +3,9 @@
 set -exu
 
 # Add your additional provisioning here for custom VM images.
+pkg update && pkg upgrade && pkg install -y git
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --default-toolchain 1.92.0
+mkdir -p /opt/sysroot/arm64
+curl -sSf https://download.freebsd.org/ftp/releases/arm64/14.3-RELEASE/base.txz | tar -C /opt/sysroot/arm64 -xJ lib usr/lib
+. "$HOME/.cargo/env"
+rustup component add rust-src
