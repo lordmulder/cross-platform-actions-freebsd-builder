f2473cabf5383af442614fc9ddb6253655828726
MuldeR <mulder2@gmx.de>
2026-01-13 22:43:22 +0100

    Workaround to free up more disk space.

--- resources/custom.sh
+++ resources/custom.sh
@@ -8,4 +8,5 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --defaul
 mkdir -p /opt/sysroot/arm64
 curl -sSf https://download.freebsd.org/ftp/releases/arm64/14.3-RELEASE/base.txz | tar -C /opt/sysroot/arm64 -xJ lib usr/lib
 . "$HOME/.cargo/env"
+rustup target add x86_64-unknown-freebsd i686-unknown-freebsd
 rustup component add rust-src
