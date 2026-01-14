9b9f1240a53e6659b007460ce148999c94e85779
MuldeR <mulder2@gmx.de>
2026-01-14 01:14:01 +0100

    Install Rust to '/opt/rust' directory, instead of '/root' directory.

--- resources/custom.sh
+++ resources/custom.sh
@@ -2,11 +2,13 @@
 
 set -exu
 
+export RUSTUP_HOME=/opt/rust/rustup
+export CARGO_HOME=/opt/rust/cargo
+
 # Add your additional provisioning here for custom VM images.
 pkg update && pkg upgrade && pkg install -y git
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --default-toolchain 1.92.0
 mkdir -p /opt/sysroot/arm64
 curl -sSf https://download.freebsd.org/ftp/releases/arm64/14.3-RELEASE/base.txz | tar -C /opt/sysroot/arm64 -xJ lib usr/lib
-. "$HOME/.cargo/env"
-rustup target add x86_64-unknown-freebsd i686-unknown-freebsd
-rustup component add rust-src
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --default-toolchain 1.92.0
+/opt/rust/cargo/bin/rustup target add x86_64-unknown-freebsd i686-unknown-freebsd
+/opt/rust/cargo/bin/rustup component add rust-src
