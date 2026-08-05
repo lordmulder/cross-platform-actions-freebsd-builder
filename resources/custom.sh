b5943f4ebf58d25445a9b364b3e928ea2461ff42
LoRd_MuldeR <mulder2@gmx.de>
2026-08-06 00:41:32 +0200

    Added the required dependencies + force update of installed packages.

--- resources/custom.sh
+++ resources/custom.sh
@@ -2,4 +2,20 @@
 
 set -exu
 
+export RUSTUP_HOME=/opt/rust/rustup
+export CARGO_HOME=/opt/rust/cargo
+
 # Add your additional provisioning here for custom VM images.
+pkg update && pkg upgrade && pkg install -y git
+mkdir -p /opt/sysroot/arm64
+
+curl -sSf https://download.freebsd.org/releases/amd64/amd64/15.1-RELEASE/lib32.txz | tar -C / -xJ usr/lib32
+curl -sSf https://download.freebsd.org/ftp/releases/arm64/15.1-RELEASE/base.txz | tar -C /opt/sysroot/arm64 -xJ lib usr/lib
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --default-toolchain 1.97.1
+
+/opt/rust/cargo/bin/rustup target add x86_64-unknown-freebsd i686-unknown-freebsd
+/opt/rust/cargo/bin/rustup component add rust-src
+
+echo '#!/bin/sh' > /opt/rust/cargo/env
+echo 'export PATH="/opt/rust/cargo/bin:$PATH"' >> /opt/rust/cargo/env
+echo 'export RUSTUP_HOME=/opt/rust/rustup' >> /opt/rust/cargo/env
